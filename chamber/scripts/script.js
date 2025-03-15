fa0c057e6ccb93bbc36ab105538ae87aa76a0213
kehnniey <kehnniey@gmail.com>
2025-03-14 22:34:32 -0400

    chamber vs2

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -28,3 +28,15 @@ function getLastModified() {
 }
 
 document.getElementById("lastModifiedMobile").innerHTML = getLastModified();
+
+
+
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
