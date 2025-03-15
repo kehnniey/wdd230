c035cd646abcec9352666ac5e3f2634e024e93c1
kehnniey <kehnniey@gmail.com>
2025-03-14 21:43:49 -0400

    chamber v1

---
+++ chamber/scripts/script.js
@@ -0,0 +1,30 @@
+// document.getElementById("year").textContent = new Date().getFullYear();
+// document.getElementById("lastModified").textContent = document.lastModified;
+
+// Set the last modified date dynamically in the footer
+const currentYearElements = document.querySelectorAll("#currentyear, .currentyear");
+const currentYear = new Date().getFullYear();
+
+currentYearElements.forEach((el) => {
+  el.textContent = currentYear;
+});
+
+
+function getLastModified() {
+  const lastModified = new Date(document.lastModified);
+  const options = {
+    weekday: 'short', 
+    year: 'numeric', 
+    month: 'short', 
+    day: 'numeric', 
+    hour: '2-digit',  
+    minute: '2-digit',
+    second: '2-digit',
+    timeZone: 'America/New_York',  
+    timeZoneName: 'short'
+  };
+  const formattedDate = lastModified.toLocaleDateString('en-US', options);
+  return `Last Modified: ${formattedDate}`;
+}
+
+document.getElementById("lastModifiedMobile").innerHTML = getLastModified();
